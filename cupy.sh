89df080f0d661813efb65ed2d7e8d4aee960a3e1
bourbonut <ben.bourbon06@gmail.com>
2022-04-27 21:46:28 +0200

    Change paths of cli tools

--- cupy.sh
+++ cupy.sh
@@ -1,1 +1,1 @@
-ncu -o profile python kcupy_lbmFlowAroundCylinder.py
+/usr/local/cuda-11.6/bin/ncu -o profile /home/nut/github/lbm-gpu/venv/bin/python kcupy_lbmFlowAroundCylinder.py
--- numba.sh
+++ numba.sh
@@ -1,1 +1,1 @@
-ncu -o profile python numba_lbmFlowAroundCylinder.py
+/usr/local/cuda-11.6/bin/ncu -o profile /home/nut/github/lbm-gpu/venv/bin/python numba_lbmFlowAroundCylinder.py
